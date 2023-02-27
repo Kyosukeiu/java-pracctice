6d6c88726b7045404954f781b1b5e5959fc5023e
Kyosukeiu <kyokyokyo0226@softbank.ne.jp>
2023-02-27 12:09:06 +0900

    2023/02/27

---
+++ jp/co/funatz/pkgTest/PackageTest.java
@@ -0,0 +1,8 @@
+package jp.co.funatz.pkgTest;
+
+public class PackageTest {
+    public static void main(String[] args) {
+        jp.co.funatz.pkgTest.a.PackageA.a(); // FQCNで指定する
+        jp.co.funatz.pkgTest.b.PackageB.b(); // FQCNで指定する
+    }
+}
